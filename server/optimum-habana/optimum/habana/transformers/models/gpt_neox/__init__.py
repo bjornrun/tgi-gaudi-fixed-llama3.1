48045dfdf8fe4b60f3628364a57045453aca5d21
Bjorn Runaker <bjornrun@gmail.com>
2024-08-14 13:07:56 +0000

    adding transformers

---
+++ server/optimum-habana/optimum/habana/transformers/models/gpt_neox/__init__.py
@@ -0,0 +1,7 @@
+from .modeling_gpt_neox import (
+    GaudiGPTNeoXForCausalLM,
+    GaudiGPTNeoXLayer,
+    gaudi_gpt_neox_attention_forward,
+    gaudi_gpt_neox_model_forward,
+    gaudi_gpt_neox_rotary_embedding_set_cos_sin_cache,
+)
